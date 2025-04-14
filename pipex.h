caf78f721a94aee61188e12a736012b596225af7
Yohen Sheraun <yosherau@u91z02s02.42kl.edu.my>
2025-04-14 19:26:50 +0800

    Tired

--- pipex.h
+++ pipex.h
@@ -3,20 +3,26 @@
 /*                                                        :::      ::::::::   */
 /*   pipex.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: yosherau <yosherau@student.42kl.edu.my>    +#+  +:+       +#+        */
+/*   By: yosherau <yosherau@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/07 15:02:33 by yosherau          #+#    #+#             */
-/*   Updated: 2025/04/10 13:13:50 by yosherau         ###   ########.fr       */
+/*   Updated: 2025/04/14 19:09:30 by yosherau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PIPEX_H
 # define PIPEX_H
 
+# include "./libft/libft.h"
 # include <stdio.h>
-# include <stdlib.h>
-# include <unistd.h>
 
+// typedef struct s_cmds
+// {
+	
+// }	t_cmds;
+
+
+char	**get_path(char *envp[]);
 void	print_error(char *msg);
 
 #endif
--- srcs/get_path.c
+++ srcs/get_path.c
@@ -1,25 +1,27 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   pipex.c                                            :+:      :+:    :+:   */
+/*   get_path.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: yosherau <yosherau@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/04/07 15:02:24 by yosherau          #+#    #+#             */
-/*   Updated: 2025/04/14 19:26:07 by yosherau         ###   ########.fr       */
+/*   Created: 2025/04/14 18:06:30 by yosherau          #+#    #+#             */
+/*   Updated: 2025/04/14 19:10:13 by yosherau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "pipex.h"
+#include "../pipex.h"
 
-int	main(int argc, char *argv[], char *envp[])
+char	**get_path(char *envp[])
 {
-	char	**path_dir;
+	char	*path;
 	int		index;
 
 	index = -1;
-	if (argc != 5)
-		print_error("Please enter the appropriate amount of inputs");
-	path_dir = get_path(envp);
-	
+	while (envp[++index])
+		if (ft_strncmp(envp[index], "PATH=", 5) == 0)
+			path = envp[index] + 5;
+	if (!path)
+		return (NULL);
+	return (ft_split(path, ':'));
 }
