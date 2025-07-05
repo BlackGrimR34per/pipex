11019fb723e612cc5f673956a01f20f183dff06b
Yohen Sheraun <yosherau@u90z08s03.42kl.edu.my>
2025-07-05 19:43:14 +0800

    Pipex bonus working

--- pipex.c
+++ pipex.c
@@ -6,12 +6,13 @@
 /*   By: yosherau <yosherau@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/07 15:02:24 by yosherau          #+#    #+#             */
-/*   Updated: 2025/06/28 15:33:16 by yosherau         ###   ########.fr       */
+/*   Updated: 2025/07/05 19:06:37 by yosherau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
+// Change the error handling when pipe fails to create a pipe
 int	handle_here_doc(char *limiter)
 {
 	int		fds[2];
@@ -40,12 +41,14 @@ int	handle_here_doc(char *limiter)
 	return (fds[0]);
 }
 
-void	handle_execution(char *argv[], int start, int input, int output)
+void	handle_execution(char *envp[], char *argv[], int start, int input, int output)
 {
 	int		argc;
 	int		index;
 	int		fds[2];
 	pid_t	pid;
+	char	**path_dir;
+	t_cmds	cmd;
 
 	argc = 0;
 	while (argv[argc])
@@ -57,16 +60,27 @@ void	handle_execution(char *argv[], int start, int input, int output)
 		pid = fork();
 		if (pid == 0)
 		{
+			path_dir = get_path(envp);
+			extract_commands(&cmd, path_dir, argv[index]);
 			dup2(input, STDIN_FILENO);
-			if (index != argc - 1)
-				dup2(fds[1], STD_OUT);
+			if (index != argc - 2)
+				dup2(fds[1], STDOUT_FILENO);
 			else
-				dup2(output, STD_OUT);
+				dup2(output, STDOUT_FILENO);
+			execve(cmd.cmd_path, cmd.args, envp);
 			close(fds[0]);
 			close(fds[1]);
 		}
+		else
+		{
+			close(fds[1]);
+			if (input != STDIN_FILENO)
+				close(input);
+			input = fds[0];
+		}
 		index++;
 	}
+	while (wait(NULL) > 0);
 }
 
 int	main(int argc, char *argv[], char *envp[])
@@ -81,27 +95,12 @@ int	main(int argc, char *argv[], char *envp[])
 	{
 		input_fd = handle_here_doc(argv[2]);
 		output_fd = open(argv[argc - 1], O_CREAT | O_WRONLY | O_APPEND);
-		handle_execution(argv, 3, input_fd, output_fd);
+		handle_execution(envp, argv, 3, input_fd, output_fd);
 	}
 	else
 	{
 		input_fd = open(argv[1], O_RDONLY);
 		output_fd = open(argv[argc - 1], O_CREAT | O_WRONLY | O_TRUNC);
-		handle_execution(argv, 2, input_fd, output_fd);
+		handle_execution(envp, argv, 2, input_fd, output_fd);
 	}
-	// if (pipe(fd) == -1)
-	// 	return (1);
-	// pid_1 = exec_child_1(envp, argv, fd);
-	// pid_2 = exec_child_2(envp, argv, fd);
-	// close(fd[0]);
-	// close(fd[1]);
-	// waitpid(pid_1, NULL, 0);
-	// waitpid(pid_2, &w_status, 0);
-	// if (WIFEXITED(w_status))
-	// {
-	// 	status_code = WEXITSTATUS(w_status);
-	// 	if (status_code != 0)
-	// 		exit(status_code);
-	// }
-	// return (0);
 }
